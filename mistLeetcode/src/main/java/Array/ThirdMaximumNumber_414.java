b3e88eb62d5aa3f4d34549f02d8605ebaba6e1df
lixiaojian <mistlxj@163.com>
2016-10-27 20:39:29 +0800

    Third Maximum Number 414

--- mistLeetcode/src/main/java/Array/ThirdMaximumNumber_414.java
+++ mistLeetcode/src/main/java/Array/ThirdMaximumNumber_414.java
@@ -1,7 +1,12 @@
 package Array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by lixiaojian on 2016/10/25.
+ *
+ * @source https://leetcode.com/problems/third-maximum-number/
  */
 public class ThirdMaximumNumber_414 {
     /*
@@ -31,31 +36,50 @@ public class ThirdMaximumNumber_414 {
     public static int thirdMax(int[] nums) {
         if(nums.length == 1) return nums[0];
         if(nums.length == 2) return Math.max(nums[0],nums[1]);
-        int max = nums[0];
-        int secMax = nums[0];
-        int thirdMax = nums[0];
-        for (int i = 1; i <nums.length ; i++) {
-            if (nums[i] > thirdMax) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i <nums.length ; i++) {
+            set.add(nums[i]);
+            if (set.size() == 3) {
+                break;
+            }
+        }
+       int[] arr = new int[3];
+        int cnt = 0;
+        for (Integer e : set) {
+            arr[cnt++] = e;
+        }
+        if (cnt==1) {
+            return arr[0];
+        }
+        if (cnt == 2) {
+            return Math.max(arr[0],arr[1]);
+        }
+
+        int max = Math.max(arr[2],Math.max(arr[0], arr[1]));
+        int thirdMax = Math.min(arr[2], Math.min(arr[0], arr[1]));
+        int secMax = arr[0] + arr[1] + arr[2] - max - thirdMax;
+
+        for(int i = 0;i<nums.length;i++) {
+            if(nums[i] == max || nums[i] == secMax || nums[i] == thirdMax) {
+                continue;
+            }else if(nums[i] > max) {
+                thirdMax = secMax;
+                secMax = max;
+                max = nums[i];
+            } else if(nums[i] > secMax) {
+                thirdMax = secMax;
+                secMax = nums[i];
+            } else if(nums[i] > thirdMax) {
                 thirdMax = nums[i];
-            }
-            if (thirdMax > secMax) {
-                swap(thirdMax,secMax);
-            }
-            if (secMax > max) {
-                swap(secMax,max);
+            }else {
+                continue;
             }
         }
         return thirdMax;
     }
 
-    private static void swap(int thirdMax, int secMax) {
-        int tmp = thirdMax;
-        thirdMax = secMax;
-        secMax = tmp;
-    }
-
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {2,2,3,1};
         System.out.println(thirdMax(arr));
     }
 
