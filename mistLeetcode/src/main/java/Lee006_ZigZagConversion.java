8a5926de5be5a736d58a3749ac3f8e8d4c9ed06b
lixiaojian <mistlxj@163.com>
2016-11-03 19:09:05 +0800

    ZigZag

---
+++ mistLeetcode/src/main/java/Lee006_ZigZagConversion.java
@@ -0,0 +1,16 @@
+/**
+ * Created by lixiaojian on 2016/11/3.
+ */
+public class Lee006_ZigZagConversion {
+
+    public String convert(String s, int numRows) {
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        //convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+
+    }
+}
